4e3e4f12a3d29b1c5f4f6767ccaa9de9214b2aeb
Elena Leusik <elena.leusik@gmail.com>
2023-08-08 22:41:42 +0200

    feat: add header layout using MUI Tabs component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,33 +1,49 @@
-import { Link, NavLink } from "react-router-dom";
+import CottageTwoToneIcon from "@mui/icons-material/CottageTwoTone";
+import HowToRegTwoToneIcon from "@mui/icons-material/HowToRegTwoTone";
+import LoginTwoToneIcon from "@mui/icons-material/LoginTwoTone";
+import { Box, Tabs, Tab } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const AppHeader = () => {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
   return (
     <header className="header">
-      <nav className="header__navigation">
-        <h1 className="header__home">
-          <Link to="/">Home</Link>
-        </h1>
-        <div className="header__right">
-          <p className="header__login">
-            <NavLink
-              end
-              style={({ isActive }) => ({ color: isActive ? "red" : "black" })}
-              to="/login"
-            >
-              Login
-            </NavLink>
-          </p>
-          <p className="header__registration">
-            <NavLink
-              end
-              style={({ isActive }) => ({ color: isActive ? "red" : "black" })}
-              to="/registration"
-            >
-              Registration
-            </NavLink>
-          </p>
-        </div>
-      </nav>
+      <Box
+        sx={{
+          mb: 2,
+          width: "95%",
+        }}
+      >
+        <Tabs value={value} onChange={handleChange} variant="fullWidth">
+          <Tab
+            value="1"
+            icon={<CottageTwoToneIcon />}
+            label="HOME"
+            to="/"
+            component={Link}
+          />
+          <Tab
+            value="2"
+            icon={<HowToRegTwoToneIcon />}
+            label="LOGIN"
+            to="/login"
+            component={Link}
+          />
+          <Tab
+            value="3"
+            icon={<LoginTwoToneIcon />}
+            label="REGISTRATION"
+            to="/registration"
+            component={Link}
+          />
+        </Tabs>
+      </Box>
     </header>
   );
 };
