bdebf5a794f335f5e7493e29398566f8a1d75ad2
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-08-09 21:17:19 +0200

    feat: :safety_vest: implement client-side validation of email and password

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,12 +1,43 @@
-// import { yupResolver } from "@hookform/resolvers/yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { TextField, Button } from "@mui/material";
 import { useForm, Controller, SubmitHandler } from "react-hook-form";
-// import * as yup from "yup";
+import * as yup from "yup";
+
+import emailValidation from "../../constants/email-validation";
 
 interface ILoginFormData {
   email: string;
   password: string;
 }
+
+const schemaLogin = yup.object({
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("This field is required")
+    .matches(emailValidation, "Enter valid email"),
+  password: yup
+    .string()
+    .required("This field is required")
+    .matches(
+      /[A-Z]/,
+      "Password must contain at least one uppercase letter (A-Z)"
+    )
+    .matches(
+      /[a-z]/,
+      "Password must contain at least one lowercase letter (a-z)"
+    )
+    .matches(/\d/, "Password must contain at least one digit (0-9)")
+    .matches(
+      /[!@#$%^&*]/,
+      "Password must contain at least one special character (!@#$%^&*)"
+    )
+    .test(
+      "noWhitespace",
+      "Password must not contain leading or trailing whitespace",
+      (value) => !value.includes(" ")
+    ),
+});
 
 const Login: React.FC = () => {
   const {
@@ -14,6 +45,7 @@ const Login: React.FC = () => {
     control,
     formState: { errors },
   } = useForm<ILoginFormData>({
+    resolver: yupResolver(schemaLogin),
     mode: "onChange",
   });
 
@@ -21,6 +53,9 @@ const Login: React.FC = () => {
     // eslint-disable-next-line no-console
     console.log(data);
   };
+
+  const emailErrorMessage = errors.email?.message;
+  const passwordErrorMessage = errors.password?.message;
 
   return (
     <div className="container">
@@ -35,6 +70,7 @@ const Login: React.FC = () => {
               fullWidth
               error={!!errors.email}
               variant="outlined"
+              helperText={emailErrorMessage}
               InputProps={{
                 ...field,
               }}
@@ -50,6 +86,7 @@ const Login: React.FC = () => {
               label="Password"
               fullWidth
               error={!!errors.password}
+              helperText={passwordErrorMessage}
               variant="outlined"
               InputProps={{
                 ...field,
