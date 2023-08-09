f2fe82c868f7beebf23a32449d0cb1763b24612e
inessa <inessasmorgunova@gmail.com>
2023-08-09 21:47:22 +0300

    feat: add button

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,3 +1,5 @@
+import CottageTwoToneIcon from "@mui/icons-material/CottageTwoTone";
+import Button from "@mui/material/Button";
 import { Link } from "react-router-dom";
 
 import styles from "./404.module.scss";
@@ -9,7 +11,17 @@ const Page404 = () => {
     <div className={styles.image}>
       <ErrorImage />
       <div className={styles.error}>Page doesn't exist</div>
-      <Link to="/">Back to main page</Link>
+      <div className={styles.button}>
+        <Link to="/">
+          <Button
+            variant="contained"
+            size="large"
+            endIcon={<CottageTwoToneIcon />}
+          >
+            Back to main page
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 };
