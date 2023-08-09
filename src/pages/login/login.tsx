aca1a9af3a2fc59dc007c78618a15d11f3ab9390
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-08-09 20:15:31 +0200

    feat: :sparkles: add basic form structure

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,73 @@
-const Login = () => {
-  return <div>This is the Login page</div>;
+// import { yupResolver } from "@hookform/resolvers/yup";
+import { TextField, Button } from "@mui/material";
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+// import * as yup from "yup";
+
+interface ILoginFormData {
+  email: string;
+  password: string;
+}
+
+const Login: React.FC = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<ILoginFormData>({
+    mode: "onChange",
+  });
+
+  const onSubmit: SubmitHandler<ILoginFormData> = (data) => {
+    // eslint-disable-next-line no-console
+    console.log(data);
+  };
+
+  return (
+    <div className="container">
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              label="Email"
+              fullWidth
+              error={!!errors.email}
+              variant="outlined"
+              InputProps={{
+                ...field,
+              }}
+            />
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextField
+              label="Password"
+              fullWidth
+              error={!!errors.password}
+              variant="outlined"
+              InputProps={{
+                ...field,
+              }}
+            />
+          )}
+        />
+        <Button
+          className="button"
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
+          Login
+        </Button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
