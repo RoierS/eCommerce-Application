bff71d8d90baa34cdca7cfb3eb7cd1e2ce2b638c
inessa <inessasmorgunova@gmail.com>
2023-08-21 16:22:10 +0300

    feat: add burger

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,55 +1,20 @@
 import React from "react";
 
-import HeaderRoute from "@components/header/header-route";
-import routes from "@components/header/tabs";
+import HeaderBurger from "@components/header/header-burger";
+import HeaderDesktop from "@components/header/header-desktop";
 
-import { Box } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 import styles from "./header.module.scss";
 
 const AppHeader = () => {
-  const hasToken = localStorage.getItem("tokenObject");
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <header className={styles.header}>
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <HeaderRoute
-          to={routes.home.link}
-          icon={routes.home.icon}
-          label={routes.home.label}
-        />
-        <Box display="flex" alignItems="center">
-          <HeaderRoute
-            to={routes.catalog.link}
-            icon={routes.catalog.icon}
-            label={routes.catalog.label}
-          />
-          <HeaderRoute
-            to={routes.cart.link}
-            icon={routes.cart.icon}
-            label={routes.cart.label}
-          />
-          {hasToken ? (
-            <HeaderRoute
-              to={routes.logout.link}
-              icon={routes.logout.icon}
-              label={routes.logout.label}
-            />
-          ) : (
-            <>
-              <HeaderRoute
-                to={routes.registration.link}
-                icon={routes.registration.icon}
-                label={routes.registration.label}
-              />
-              <HeaderRoute
-                to={routes.login.link}
-                icon={routes.login.icon}
-                label={routes.login.label}
-              />
-            </>
-          )}
-        </Box>
-      </Box>
+      {isMobile ? <HeaderBurger /> : <HeaderDesktop />}
     </header>
   );
 };
