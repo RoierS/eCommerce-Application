afa34f2f7f5e0e71117117d01579312acde83d81
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2023-09-01 21:30:51 +0200

    refactor: divide IProductData interface into small ones

--- src/interfaces/product-search-result.ts
+++ src/interfaces/product-search-result.ts
@@ -1,46 +1,58 @@
+// export interface IProductSearchResult {
+//   id: string;
+//   name: {
+//     "en-US": string;
+//   };
+//   description: {
+//     "en-US": string;
+//   };
+//   categories: {
+//     typeId: string;
+//     id: string;
+//   }[];
+//   masterVariant: {
+//     attributes: {
+//       name: string;
+//       value: string;
+//     }[];
+//     images: {
+//       url: string;
+//       dimensions: {
+//         w: number;
+//         h: number;
+//       };
+//     }[];
+//     prices: {
+//       id: string;
+//       value: {
+//         type: string;
+//         currencyCode: string;
+//         centAmount: number;
+//         fractionDigits: number;
+//       };
+//       discounted?: {
+//         value: {
+//           type: string;
+//           currencyCode: string;
+//           centAmount: number;
+//           fractionDigits: number;
+//         };
+//       };
+//     }[];
+//     key: string;
+//     sku: string;
+//     id: number;
+//   };
+
+import { ICategory, ILocalizedText, IMasterVariant } from "./product-data";
+
+// }
 export interface IProductSearchResult {
   id: string;
-  name: {
-    "en-US": string;
-  };
-  description: {
-    "en-US": string;
-  };
-  categories: {
-    typeId: string;
-    id: string;
-  }[];
-  masterVariant: {
-    attributes: {
-      name: string;
-      value: string;
-    }[];
-    images: {
-      url: string;
-      dimensions: {
-        w: number;
-        h: number;
-      };
-    }[];
-    prices: {
-      id: string;
-      value: {
-        type: string;
-        currencyCode: string;
-        centAmount: number;
-        fractionDigits: number;
-      };
-      discounted?: {
-        value: {
-          type: string;
-          currencyCode: string;
-          centAmount: number;
-          fractionDigits: number;
-        };
-      };
-    }[];
-    key: string;
-    sku: string;
-    id: number;
-  };
+  name: ILocalizedText;
+  description: ILocalizedText;
+  categories: ICategory[];
+  masterVariant: IMasterVariant;
+  key: string;
+  sku: string;
 }
