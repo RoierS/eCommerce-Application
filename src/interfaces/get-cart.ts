5cfb17e6cf6964851aa829082d899165dbf645a9
inessa <inessasmorgunova@gmail.com>
2023-09-14 06:41:29 +0300

    feat: add get cart interface

---
+++ src/interfaces/get-cart.ts
@@ -0,0 +1,22 @@
+import { IImage, IPrice, IVariant } from "@interfaces/product-response";
+
+enum Locale {
+  EnUS = "en-US",
+}
+
+interface ITranslation {
+  [Locale.EnUS]: string;
+}
+export interface ILineItem {
+  variant: IVariant;
+  images: IImage[];
+  name: ITranslation;
+  price: IPrice;
+
+  productId: string;
+}
+export interface ICartResponse {
+  version: number;
+  lineItems: ILineItem[];
+  id: number;
+}
