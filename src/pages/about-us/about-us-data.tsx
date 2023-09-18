d499be5c98a25f11ad71910d1ba8a833c57b15ce
inessa <inessasmorgunova@gmail.com>
2023-09-18 08:13:05 +0300

    feat: add about us data

---
+++ src/pages/about-us/about-us-data.tsx
@@ -0,0 +1,122 @@
+import InesaImage from "@assets/images/about-us/inesa.jpg";
+
+const cardsData = [
+  {
+    name: "Inesa Smarhunova",
+    jobTitle: "Junior Frontend Developer",
+    skills: [
+      "HTML5, CSS3",
+      "JavaScript Basics",
+      "Git, GitHub",
+      "React",
+      "TypeScript",
+    ],
+    education: "Vitebsk State Order of Peoples' Friendship Medical University",
+    courses: [
+      {
+        name: "Полный курс по JavaScript + React",
+        link: "https://www.udemy.com/course/javascript_full/",
+      },
+      {
+        name: "Build Responsive Real-World Websites with HTML and CSS",
+        link: "https://www.udemy.com/course/design-and-develop-a-killer-website-with-html5-and-css3/",
+      },
+      {
+        name: "RS School (In process): Course JavaScript/Front-end",
+        link: "https://rs.school/index.html",
+      },
+    ],
+    englishLevel: "EPAM English test result - B2",
+    githubProfile: "https://github.com/papryca",
+    imageSrc: InesaImage,
+    implemented: [
+      "Create the project in CommerceTools",
+      "Implement routing for navigation",
+      "Implement a 404 (Not Found) page",
+      "Display Product Information",
+      "Image Modal with Slider",
+      "Header",
+      "Add or remove product from cart",
+      "Introduction to the Development team",
+    ],
+  },
+  {
+    name: "Inesa Smarhunova",
+    jobTitle: "Junior Frontend Developer",
+    skills: [
+      "HTML5, CSS3",
+      "JavaScript Basics",
+      "Git, GitHub",
+      "React",
+      "TypeScript",
+    ],
+    education: "Vitebsk State Order of Peoples' Friendship Medical University",
+    courses: [
+      {
+        name: "Полный курс по JavaScript + React",
+        link: "https://www.udemy.com/course/javascript_full/",
+      },
+      {
+        name: "Build Responsive Real-World Websites with HTML and CSS",
+        link: "https://www.udemy.com/course/design-and-develop-a-killer-website-with-html5-and-css3/",
+      },
+      {
+        name: "Course JavaScript/Front-end",
+        link: "https://rs.school/index.html",
+      },
+    ],
+    englishLevel: "EPAM English test result - B2",
+    githubProfile: "https://github.com/papryca",
+    imageSrc: InesaImage,
+    implemented: [
+      "Create the project in CommerceTools",
+      "Implement routing for navigation",
+      "Implement a 404 (Not Found) page",
+      "Display Product Information",
+      "Image Modal with Slider",
+      "Header",
+      "Add or remove product from cart",
+      "Introduction to the Development team",
+    ],
+  },
+  {
+    name: "Inesa Smarhunova",
+    jobTitle: "Junior Frontend Developer",
+    skills: [
+      "HTML5, CSS3",
+      "JavaScript Basics",
+      "Git, GitHub",
+      "React",
+      "TypeScript",
+    ],
+    education: "Vitebsk State Order of Peoples' Friendship Medical University",
+    courses: [
+      {
+        name: "Полный курс по JavaScript + React",
+        link: "https://www.udemy.com/course/javascript_full/",
+      },
+      {
+        name: "Build Responsive Real-World Websites with HTML and CSS",
+        link: "https://www.udemy.com/course/design-and-develop-a-killer-website-with-html5-and-css3/",
+      },
+      {
+        name: "Course JavaScript/Front-end",
+        link: "https://rs.school/index.html",
+      },
+    ],
+    englishLevel: "EPAM English test result - B2",
+    githubProfile: "https://github.com/papryca",
+    imageSrc: InesaImage,
+    implemented: [
+      "Create the project in CommerceTools",
+      "Implement routing for navigation",
+      "Implement a 404 (Not Found) page",
+      "Display Product Information",
+      "Image Modal with Slider",
+      "Header",
+      "Add or remove product from cart",
+      "Introduction to the Development team",
+    ],
+  },
+];
+export default cardsData;
